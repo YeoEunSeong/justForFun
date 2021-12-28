77fc81d2557d2f01eda37040c995f6c569152af3
Eunseong, Yeo <e7838752@gmail.com>
2021-12-28 19:48:03 +0900

    ✨ feat: random cat generator

---
+++ random-image/js/app.js
@@ -0,0 +1,20 @@
+const catGenerator = (() => {
+  const LAST_NUMBER = 10;
+  const $image = document.querySelector('.cat-image');
+
+  let prevImage = 0;
+
+  return {
+    nextImage() {
+      let randomNumber = Math.floor(Math.random() * LAST_NUMBER) + 1;
+      while (randomNumber === prevImage) {
+        randomNumber = Math.floor(Math.random() * LAST_NUMBER) + 1;
+      }
+
+      prevImage = randomNumber;
+      $image.src = `img/image${(randomNumber < 10 ? '0' : '') + randomNumber}.jpeg`;
+    },
+  };
+})();
+
+document.querySelector('.next').onclick = catGenerator.nextImage;
